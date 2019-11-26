979fd0b8ee7e392075e97fc6c07707e047bafa83
Sabas Vega <sabascarloseduardo@gmail.com>
2019-11-25 22:02:55 -0400

    checkout of store

--- _config/frontendPages.js
+++ _config/frontendPages.js
@@ -11,5 +11,16 @@ export default {
     icon: 'fas fa-layer-group',
     //authenticated: true
   },
+  checkout: {
+    permission: null,
+    activated: true,
+    path: 'marketplace/checkout',
+    name: 'marketplace.checkout',
+    page: () => import('@imagina/qmarketplace/_layouts/frontend/checkout/index'),
+    layout: () => import('src/layouts/master'),
+    title: 'qmarketplace.sidebar.checkout',
+    icon: 'fas fa-layer-group',
+    //authenticated: true
+  },
 
 }
