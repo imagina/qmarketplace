b57e7da0d38b657fbbdcc679badc2532a15eeaa7
Adriana <acardosof27@gmail.com>
2019-12-23 18:56:55 -0400

    notifications

--- _layouts/admin/notification/notifications.vue
+++ _layouts/admin/notification/notifications.vue
@@ -1,6 +1,60 @@
 <template>
   <div id="pageNotifications"
-       class="q-layout-page row layout-padding q-container">
+       class=" q-py-xl q-container">
+
+    <div class="row">
+      <div class="col-12">
+        <div class="text-h5 text-primary q-mb-md font-family-secondary">Notificaciones</div>
+      </div>
+      <div class="col-12">
+
+        <q-card class="rounded-md bg-white q-mb-xl">
+
+          <q-card-section class="q-pa-lg">
+
+
+            <ul>
+              <li>
+                <div class="row q-col-gutter-md items-center">
+                  <div class="col text-truncate">
+                    <span class="text-primary text-bold ">1. </span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas architecto, temporibus odio provident veniam rem, impedit magni minima doloribus natus repellat dignissimos asperiores similique, enim quae, error aut. Non, quo!
+                    </div>
+                    <div class="col-auto">
+                      <q-btn dense round icon="fas fa-eye" class="q-mr-sm q-pa-xs" size="10px" color="primary" />
+                      <q-btn dense round icon="fas fa-trash-alt" class="q-mr-sm q-pa-xs" size="10px" color="primary" />
+                    </div>
+                </div> 
+              </li>
+              <li>
+                <div class="row q-col-gutter-md items-center">
+                  <div class="col text-truncate">
+                    <span class="text-primary text-bold ">2. </span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas architecto, temporibus odio provident veniam rem, impedit magni minima doloribus natus repellat dignissimos asperiores similique, enim quae, error aut. Non, quo!
+                    </div>
+                    <div class="col-auto">
+                      <q-btn dense round icon="fas fa-eye" class="q-mr-sm q-pa-xs" size="10px" color="primary" />
+                      <q-btn dense round icon="fas fa-trash-alt" class="q-mr-sm q-pa-xs" size="10px" color="primary" />
+                    </div>
+                </div>  
+              </li>
+              <li>
+                <div class="row q-col-gutter-md items-center">
+                  <div class="col text-truncate">
+                    <span class="text-primary text-bold ">3. </span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas architecto, temporibus odio provident veniam rem, impedit magni minima doloribus natus repellat dignissimos asperiores similique, enim quae, error aut. Non, quo!
+                    </div>
+                    <div class="col-auto">
+                      <q-btn dense round icon="fas fa-eye" class="q-mr-sm q-pa-xs" size="10px" color="primary" />
+                      <q-btn dense round icon="fas fa-trash-alt" class="q-mr-sm q-pa-xs" size="10px" color="primary" />
+                    </div>
+                </div> 
+              </li>
+            </ul>
+
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+ 
+      
   </div>
 </template>
 <script>
@@ -43,4 +97,13 @@
       padding 8px 0 8px 35px
       .mesageNotification
         padding 15px
+
+    ul 
+      list-style none
+      padding-left 0
+      li    
+        border 1px solid $primary
+        padding 20px
+        border-radius 10px
+        margin-bottom 20px
 </style>
