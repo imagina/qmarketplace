75facae98c6274a61d310844965f1fd896cbc064
Sabas Vega <sabascarloseduardo@gmail.com>
2020-04-16 18:35:12 -0400

    Fix error next button in quiz component and added validation of status 1 & start/end date of question

--- _components/themes/quiz.vue
+++ _components/themes/quiz.vue
@@ -1,203 +1,232 @@
 <template>
-   <q-card class="card-quiz-1 rounded-sm full-width q-mb-xl" v-if="success">
+  <q-card class="card-quiz-1 rounded-sm full-width q-mb-xl" v-if="success">
 
-      <div class="q-card-title bg-store-primary rounded-sm text-center text-white q-py-sm">
-         ENCUESTA
+    <div class="q-card-title bg-store-primary rounded-sm text-center text-white q-py-sm">
+      ENCUESTA
+    </div>
+    <q-stepper ref="stepper"
+    v-model="currentStep" class="no-shadow" v-if="poll && poll.questions.length">
+
+    <q-step :name="`question-${index}`" :order="index" :title="question.title" v-for="(question, index) in questions"
+    :key="index">
+    <q-card-section>
+
+      <div class="text-subtitle1 text-bold text-center q-mb-sm">{{question.title}}</div>
+
+      <div v-if="!question.users.find(element => element == userId)">
+        <q-option-group keep-color
+        v-model="selectedOption"
+        :options="getAnswers(question.answers)"
+        color="store-primary"
+
+        />
+        <q-stepper-navigation align="center" class="q-pa-md">
+          <q-btn class="bg-store-primary text-light font-family-secondary" @click="saveData(question)">Enviar
+          </q-btn>
+        </q-stepper-navigation>
+
       </div>
-      <q-stepper ref="stepper"
-                 v-model="currentStep" class="no-shadow" v-if="poll && poll.questions.length">
+      <div v-else>
+        <div class="question q-mb-md">
+          <table class="q-mx-auto">
+            <thead>
+              <tr>
+                <th style="width:50%;"></th>
+                <th class="text-center" style="width:20%;">Votos</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="answers" v-for="(answer) in question.answers" :key="answer.id">
+                <td>{{answer.title}}</td>
+                <td class="text-center"> {{answer.votes}}</td>
+              </tr>
+              <tr>
+                <td>
+                  TOTAL
+                </td>
+                <td class="text-center">
+                  {{question.totalVotes}}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
 
-         <q-step :name="`question-${index}`" :order="index" :title="question.title" v-for="(question, index) in poll.questions"
-                 :key="index">
-            <q-card-section>
+    </q-card-section>
 
-               <div class="text-subtitle1 text-bold text-center q-mb-sm">{{question.title}}</div>
-
-               <div v-if="!question.users.find(element => element == userId)">
-                  <q-option-group keep-color
-                                  v-model="selectedOption"
-                                  :options="getAnswers(question.answers)"
-                                  color="store-primary"
-
-                  />
-                  <q-stepper-navigation align="center" class="q-pa-md">
-                     <q-btn class="bg-store-primary text-light font-family-secondary" @click="saveData(question)">Enviar
-                     </q-btn>
-                  </q-stepper-navigation>
-
-               </div>
-               <div v-else>
-                     <div class="question q-mb-md">
-                        <table class="q-mx-auto">
-                           <thead>
-                           <tr>
-                              <th style="width:50%;"></th>
-                              <th class="text-center" style="width:20%;">Votos</th>
-                           </tr>
-                           </thead>
-                           <tbody>
-                           <tr class="answers" v-for="(answer) in question.answers" :key="answer.id">
-                              <td>{{answer.title}}</td>
-                              <td class="text-center"> {{answer.votes}}</td>
-                           </tr>
-                           <tr>
-                              <td>
-                                 TOTAL
-                              </td>
-                              <td class="text-center">
-                                 {{question.totalVotes}}
-                              </td>
-                           </tr>
-                           </tbody>
-                        </table>
-                     </div>
-                  </div>
-
-            </q-card-section>
-
-            <q-stepper-navigation align="center" class="q-pa-md" v-if="index < poll.questions.length - 1">
-               <div class="text-store-primary text-center cursor-pointer" @click="next()">
-                  <div class="font-family-secondary">Siguiente</div>
-               </div>
-            </q-stepper-navigation>
+    <q-stepper-navigation align="center" class="q-pa-md" v-if="index < questions.length - 1">
+      <div class="text-store-primary text-center cursor-pointer" @click="$refs.stepper.next()">
+        <div class="font-family-secondary">Siguiente</div>
+      </div>
+    </q-stepper-navigation>
 
 
-         </q-step>
-      </q-stepper>
-      <q-card-section v-else-if="!alertContent.active" class="q-py-xl">
-         <div class="font-family-secondary text-center">
-            No hay encuestas activas
-         </div>
-      </q-card-section>
-      <!-- msj final -->
-      <q-card-section v-if="alertContent.active && !showVotes" class="q-py-xl">
-         <div class="font-family-secondary text-center">
-            {{alertContent.msj}}
-         </div>
-      </q-card-section>
-   </q-card>
+  </q-step>
+</q-stepper>
+<q-card-section v-else-if="!alertContent.active" class="q-py-xl">
+  <div class="font-family-secondary text-center">
+    No hay encuestas activas
+  </div>
+</q-card-section>
+<!-- msj final -->
+<q-card-section v-if="alertContent.active && !showVotes" class="q-py-xl">
+  <div class="font-family-secondary text-center">
+    {{alertContent.msj}}
+  </div>
+</q-card-section>
+</q-card>
 
 
 </template>
 <script>
-   //Plugins
-   import {required} from 'vuelidate/lib/validators'
+//Plugins
+import {required} from 'vuelidate/lib/validators'
 
-   export default {
-      props: ['systemName'],
-      validations() {
-         return {
-            selectedOption: {required}
-         }
+export default {
+  props: ['systemName'],
+  validations() {
+    return {
+      selectedOption: {required}
+    }
+  },
+  beforeMount() {
+    this.$nextTick(function () {
+      this.init()
+    })
+  },
+  data() {
+    return {
+      loading: true,
+      success: false,
+      pollUserIds: [],
+      questions: [],
+      polls: [],
+      poll: false,
+      selectedOption: null,
+      finalDataSave: [],
+      userId: this.$store.state.quserAuth.userId ? this.$store.state.quserAuth.userId : null,
+      alertContent: {
+        active: false,
+        color: 'bg-secondary',
+        icon: 'check',
+        msj: 'Gracias por participar!!'
       },
-      beforeMount() {
-         this.$nextTick(function () {
-            this.init()
-         })
-      },
-      data() {
-         return {
-            loading: true,
-            success: false,
-            pollUserIds: [],
-            polls: [],
-            poll: false,
-            selectedOption: null,
-            finalDataSave: [],
-            userId: this.$store.state.quserAuth.userId ? this.$store.state.quserAuth.userId : null,
-            alertContent: {
-               active: false,
-               color: 'bg-secondary',
-               icon: 'check',
-               msj: 'Gracias por participar!!'
+      currentStep: "question-0",
+      votesPoll: null,
+      showVotes: false
+    }
+  },
+  computed: {
+    storeData() {
+      let storeSlug = this.$route.params.slug
+
+      return this.$store.state.qcrudMaster.show[`qmarketplace-store-${storeSlug}`].data
+    },
+  },
+  methods: {
+    // Init Method
+    async init() {
+      await this.getPoll()
+    },
+    validateDateWithNow(dateFrom,dateTo){
+      var d = new Date();
+      var day=d.getDate();
+      var month=d.getMonth();
+      var year=d.getFullYear();
+      var now=day+"/"+month+"/"+year;//DD-MM-YYYY
+      var d1 = dateFrom.split("-");//YYYY-MM-DD
+      var d2 = dateTo.split("-");//YYYY-MM-DD
+      var from = new Date(d1[0], parseInt(d1[1])-1, d1[2]);  // -1 because months are from 0 to 11
+      var to   = new Date(d2[0], parseInt(d2[1])-1, d2[2]);
+      var check = new Date(year, month, day);
+      console.log(check);
+      console.log(from);
+      console.log(to);
+      if(check >= from && check < to){
+        console.log(true);
+        return true;
+      }else {
+        console.log(false);
+        return false;
+      }
+    },
+    getPoll() {
+      return new Promise((resolve, reject) => {
+        //Params
+        let params = {
+          params: {
+            include: 'questions',
+            filter: {
+              storeId: this.storeData.id,
+              status: 1,
+              field: 'system_name'
             },
-            currentStep: "question-0",
-            votesPoll: null,
-            showVotes: false
-         }
-      },
-      computed: {
-         storeData() {
-            let storeSlug = this.$route.params.slug
+          }
+        }
+        this.$crud.show("apiRoutes.qquiz.polls", this.systemName, params)
+        .then(response => {
+          this.poll = response.data
+          for(var i=0;i<this.poll.questions.length;i++){
+            if(this.validateDateWithNow(this.poll.questions[i].startDate,this.poll.questions[i].endDate) && this.poll.questions[i].status==1){
+              this.questions.push(this.poll.questions[i])
+            }//if
+          }//for
 
-            return this.$store.state.qcrudMaster.show[`qmarketplace-store-${storeSlug}`].data
-         },
-      },
-      methods: {
-         // Init Method
-         async init() {
-            await this.getPoll()
-         },
-         getPoll() {
-            return new Promise((resolve, reject) => {
-               //Params
-               let params = {
-                  params: {
-                     include: 'questions',
-                     filter: {
-                        storeId: this.storeData.id,
-                        status: 1,
-                        field: 'system_name'
-                     },
-                  }
-               }
-               this.$crud.show("apiRoutes.qquiz.polls", this.systemName, params)
-                   .then(response => {
-                      this.poll = response.data
-                      this.success=true
-                      resolve(true)//Resolve
-                   }).catch(error => {
-                  this.success=true
-                  reject(false)//Resolve
-               })
+          this.success=true
+          resolve(true)//Resolve
+        }).catch(error => {
+          this.success=true
+          reject(false)//Resolve
+        })
 
-            })
-         },
-         saveData(question) {
-            this.$v.$touch()
-            if (!this.$v.$error) {
+      })
+    },
+    saveData(question) {
+      this.$v.$touch()
+      if (!this.$v.$error) {
 
-               this.loading = true;
-               let data={
-                  'user_id':this.userId,
-                  'question_id':question.id,
-                  'answer_id':this.selectedOption
-               }
+        this.loading = true;
+        let data={
+          'user_id':this.userId,
+          'question_id':question.id,
+          'answer_id':this.selectedOption
+        }
 
-               this.$crud.create('apiRoutes.qquiz.userQuestionAnswers', data).then(response => {
-                  this.$alert.info({message: 'Encuesta enviada'})
-                  this.getPoll()
-                  this.$v.$reset()
-                  this.showVotes = true
-                  this.loading = false;
+        this.$crud.create('apiRoutes.qquiz.userQuestionAnswers', data).then(response => {
+          this.$alert.info({message: 'Encuesta enviada'})
+          this.getPoll()
+          this.$v.$reset()
+          this.showVotes = true
+          this.loading = false;
 
-               }).catch(error => {
-                  console.error('[CREATE USER QUESTION ANSWERS] ', error)
-                  this.$alert.error({message: this.$tr('ui.message.recordNoUpdated')})
-                  this.loading = false
-               })
+        }).catch(error => {
+          console.error('[CREATE USER QUESTION ANSWERS] ', error)
+          this.$alert.error({message: this.$tr('ui.message.recordNoUpdated')})
+          this.loading = false
+        })
 
-            } else {
-               this.$alert.error({message: 'Encuesta: Debe seleccionar una respuesta', pos: 'bottom'})
-            }
-
-         },
-         getAnswers(arrary) {
-            let options = [];
-            arrary.forEach((answer, index) => {
-               options.push({
-                  label: answer.title,
-                  value: answer.id
-               })
-            });
-            return options;
-
-         },
-
-
+      } else {
+        this.$alert.error({message: 'Encuesta: Debe seleccionar una respuesta', pos: 'bottom'})
       }
 
-   }
+    },
+    getAnswers(arrary) {
+      let options = [];
+      arrary.forEach((answer, index) => {
+        options.push({
+          label: answer.title,
+          value: answer.id
+        })
+      });
+      return options;
+
+    },
+
+
+  }
+
+}
 </script>
 <style lang="stylus">
    .card-quiz-1
